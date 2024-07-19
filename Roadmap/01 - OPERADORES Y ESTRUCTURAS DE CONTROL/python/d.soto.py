708f97c99bee76979cf4b0c83459dfdf65bf3725
d.soto <d.soto@diagonalcompany.com>
2024-07-19 10:41:42 +0200

    Carga de ejercicio "Actividad 2"

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/d.soto.py
@@ -0,0 +1,43 @@
+# Ejercicio
+
+"""
+ * - Crea ejemplos utilizando todos los tipos de operadores de tu lenguaje:
+ *   Aritméticos, lógicos, de comparación, asignación, identidad, pertenencia, bits...
+ *   (Ten en cuenta que cada lenguaje puede poseer unos diferentes)
+ * - Utilizando las operaciones con operadores que tú quieras, crea ejemplos
+ *   que representen todos los tipos de estructuras de control que existan
+ *   en tu lenguaje:
+ *   Condicionales, iterativas, excepciones...
+ * - Debes hacer print por consola del resultado de todos los ejemplos.
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Crea un programa que imprima por consola todos los números comprendidos
+ * entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3.
+ *
+ * Seguro que al revisar detenidamente las posibilidades has descubierto algo nuevo.
+
+"""
+
+# Operadores Arimeticos
+print('''----
+Operadores Arimeticos
+----''')
+print("2+3=",2+3)
+print("2-3=",2-3)
+print("2*3=",2*3)
+print("2/3=",2/3)
+print("2%3=",2%3)
+print("2**3=",2**3)
+print("2//3=",2//3)
+
+# Operadores de comparación
+print('''----
+Operadores comprobación
+----''')
+print("2==3=",2==3)
+print("2!=",2!=3)
+print("2>3=",2>3)
+print("2<3=",2<3)
+print("2>=3=",2>=3)
+print("2<=3=",2<=3)
+
